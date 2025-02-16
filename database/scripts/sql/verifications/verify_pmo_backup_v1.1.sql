f99e39ec5922fb3aeec02cb7a8aec8201be5978e
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-02-15 17:12:03 -0800

    Corrected 'market_name' to 'market' to align with pmo backup table structure

--- database/scripts/sql/verifications/verify_pmo_backup_v1.1.sql
+++ database/scripts/sql/verifications/verify_pmo_backup_v1.1.sql
@@ -13,16 +13,16 @@ GROUP BY budget_account, project_no
 ORDER BY budget_account;
 
 -- Verify region name reassignment for North America (Canada & U.S.)
-SELECT DISTINCT market_name, region_name
+SELECT DISTINCT market, region_name
 FROM pmo_backup
-WHERE market_name IN ('Canada', 'United States')
-ORDER BY region_name, market_name;
+WHERE market IN ('Canada', 'United States')
+ORDER BY region_name, market;
 
 -- Verify region name reassignment for Corporate (Tik Tok & ByteDance Corporate)
-SELECT DISTINCT market_name, region_name
+SELECT DISTINCT market, region_name
 FROM pmo_backup
-WHERE market_name IN ('Tik Tok', 'ByteDance Corporate')
-ORDER BY region_name, market_name;
+WHERE market IN ('Tik Tok', 'ByteDance Corporate')
+ORDER BY region_name, market;
 
 -- Verify company name correction from 42Weeks to 42West
 SELECT DISTINCT company_name
@@ -30,11 +30,11 @@ FROM pmo_backup
 WHERE company_name ILIKE '42West';
 
 -- Verify Ireland’s region change from U.K. to Europe
-SELECT DISTINCT market_name, region_name
+SELECT DISTINCT market, region_name
 FROM pmo_backup
-WHERE market_name = 'Ireland';
+WHERE market = 'Ireland';
 
 -- Verify London’s update to 'Regional Great Britain' in 'U.K.' region
-SELECT DISTINCT market_name, region_name
+SELECT DISTINCT market, region_name
 FROM pmo_backup
-WHERE market_name = 'Regional Great Britain';
+WHERE market = 'Regional Great Britain';
