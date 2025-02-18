e9cec6032e4bcf151d0f316682762c39e3b7bbd9
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-02-18 09:56:49 -0800

    Renamed SQL script to campaigns_schema_update_v1.2.sql from agency_contracts_schema_update_v1.1.sql (incorrect)

---
+++ database/migrations/campaigns_schema_update_v1.2.sql
@@ -0,0 +1,15 @@
+-- ==============================================
+-- SCHEMA UPDATE: Dropped company_name and company_legal_entity from agency_contracts table
+-- ==============================================
+-- Author: Katherina Dawkins  
+-- Date: 2025-02-18  
+-- Purpose: Drops company_name and company_legal_entity to normalize and optimize database querying
+-- ==============================================
+
+BEGIN;
+
+ALTER TABLE campaigns
+DROP COLUMN company_name,
+DROP COLUMN company_legal_entity;
+
+COMMIT;
