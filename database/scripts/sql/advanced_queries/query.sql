3dc86995271f118e5fac3c944c804bb81ebf9758
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-02-25 15:17:26 -0800

    Initated database\scripts\sql\advanced_queries\query.sql

--- database/scripts/sql/advanced_queries/query.sql
+++ database/scripts/sql/advanced_queries/query.sql
@@ -1,1 +1,14 @@
-SELECT * FROM public.campaigns LIMIT 5;
+-- ==============================================
+-- ADVANCED QUERYING: COMPLEX JOINS
+-- ==============================================
+-- Author: Katherina Dawkins
+-- Date: 2025-02-25
+-- Purpose: Demonstrate query writing fluency
+-- ==============================================
+
+-- Adding/Reconnectiong PostgreSQL extension (Chris Kolkam)
+-- Testing public.budget database connection to VS Code
+SELECT current_database(), current_user;
+SELECT * FROM campaigns LIMIT 5;
+
+
